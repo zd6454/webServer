cba1ee761e84a29e90894eb1d746bd98a65fdd85
zd6454 <645405513@qq.com>
2021-06-27 20:56:30 +0800

    1、合并校友会

---
+++ src/pages/collegeFriend/structural/index.jsx
@@ -0,0 +1,60 @@
+import React,{Component} from 'react'
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import request from 'umi-request';
+import { message} from 'antd';
+import ZdTextArea from '../../components/ZdTextArea'
+
+class Index extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state={
+          data:''
+        }
+    }
+    componentDidMount(){
+        this.getMessage();
+     }
+     getMessage=()=>{
+       request('http://duing.site:2333/information/getInforContent/SchoolmateArchitecture', {
+             method: 'GET',
+           })
+             .then((response)=> {
+               console.log(response)
+               this.setState({
+                   data:response
+               })
+             })
+             .catch((error)=> {
+               message.error('信息获取失败')
+             });
+     }
+     sendMessage=(params)=>{
+         request('http://duing.site:2333/information/addInformation/SchoolmateArchitecture', {
+             method: 'POST',
+             data:params,
+           })
+             .then(function(response) {
+               message.success('信息保存成功')
+             })
+             .catch(function(error) {
+               message.error('信息保存失败')
+             });
+     }
+    render(){
+        const{data}=this.state;
+        return(
+            <PageHeaderWrapper>
+              <div>
+                  <ZdTextArea
+                    data={data}
+                    imgUrl={'http://duing.site:2333/information/uploadFile/SchoolmateArchitecture'}
+                    comfirm={this.sendMessage}
+                  />
+              </div>
+
+            </PageHeaderWrapper>
+        )
+    }
+}
+export default Index;
